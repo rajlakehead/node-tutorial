70b7d11d20996c8d597169f2219a3a3e49f3959b
RAJ PATEL <patelraj24365@gmail.com>
2023-10-03 00:42:13 -0400

    added new pages

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req, res) =>{
     console.log("Request made")
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 400;
+            break;   
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile(path, (err, data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            //res.write(data);
+            res.end(data);
+        }
+    })
+
 });
 
-res.set
 
 server.listen(3000, 'localhost', () =>{
     console.log("listning on 3000 port");
